4ff245defe8d5afb2c8434a3ea5e9dcc51f5b22c
Rossb0b <n.hallaert@myunisoft.fr>
2023-01-13 16:23:15 +0100

    test(./test/UT/class/dispatcher): enhanced ut

--- test/UT/class/dispatcher.spec.ts
+++ test/UT/class/dispatcher.spec.ts
@@ -63,23 +63,23 @@ describe("Dispatcher without options", () => {
 
     await channel.publish({ foo: "bar" });
 
-    await new Promise((resolve) => setImmediate(resolve));
+    await new Promise((resolve) => setTimeout(resolve, 1_000));
 
     expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Malformed message"));
     expect(mockedHandleDispatcherMessages).not.toHaveBeenCalled();
     expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
   });
 
-  test("Publishing an unknown event, it should log a new Error", async() => {
+  test("Publishing an Malformed message, it should log a new Error", async() => {
     const channel = new Channel({
       name: "dispatcher"
     });
 
     await channel.publish({ event: "foo" });
 
-    await new Promise((resolve) => setImmediate(resolve));
+    await new Promise((resolve) => setTimeout(resolve, 1_000));
 
-    expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Unknown Event"));
+    expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Malformed message"));
     expect(mockedHandleDispatcherMessages).not.toHaveBeenCalled();
     expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
   });
@@ -106,40 +106,152 @@ describe("Dispatcher with injected schemas", () => {
     await dispatcher.close();
   });
 
-  beforeEach(() => {
-    mockedLoggerError.mockClear();
-    mockedLoggerInfo.mockClear();
-    mockedHandleDispatcherMessages.mockClear();
-    mockedHandleIncomerMessages.mockClear();
-  });
-
   test("Dispatcher should be defined", () => {
     expect(dispatcher).toBeInstanceOf(Dispatcher);
     expect(dispatcher.prefix).toBe("");
     expect(dispatcher.privateUuid).toBeDefined();
   });
 
-  test("Publishing a malformed message, it should log a new Error", async() => {
+  describe("On any channel", () => {
+    beforeEach(() => {
+      mockedLoggerError.mockClear();
+      mockedLoggerInfo.mockClear();
+      mockedHandleDispatcherMessages.mockClear();
+      mockedHandleIncomerMessages.mockClear();
+    });
+
+    test(`Publishing a message without event,
+          it should log a new Error with the message "Malformed message"`,
+    async() => {
+      const channel = new Channel({
+        name: "dispatcher"
+      });
+
+      await channel.publish({
+        data: {
+          foo: "bar"
+        },
+        metadata: {
+          origin: "foo",
+          transactionId: "bar"
+        }
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Malformed message"));
+      expect(mockedHandleDispatcherMessages).not.toHaveBeenCalled();
+      expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
+    });
+
+    test(`Publishing a message without metadata,
+          it should log a new Error with the message "Malformed message"`,
+    async() => {
+      const channel = new Channel({
+        name: "dispatcher"
+      });
+
+      await channel.publish({
+        event: "foo",
+        data: {
+          foo: "bar"
+        }
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Malformed message"));
+      expect(mockedHandleDispatcherMessages).not.toHaveBeenCalled();
+      expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
+    });
+
+    test("Publishing an unknown event, it should log a new Error with the message `Unknown Event`", async() => {
+      const channel = new Channel({
+        name: "dispatcher"
+      });
+
+      await channel.publish({
+        event: "bar",
+        data: {
+          foo: "bar"
+        },
+        metadata: {
+          origin: "foo",
+          transactionId: "bar"
+        }
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Unknown Event"));
+      expect(mockedHandleDispatcherMessages).not.toHaveBeenCalled();
+      expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("Publishing on the dispatcher channel", () => {
+    beforeEach(() => {
+      mockedLoggerError.mockClear();
+      mockedLoggerInfo.mockClear();
+      mockedHandleDispatcherMessages.mockClear();
+      mockedHandleIncomerMessages.mockClear();
+    });
+
+    test(`Publishing an unknown event on the dispatcher channel,
+          it should log a new Error with the message "Unknown event on dispatcher channel"`,
+    async() => {
+      const channel = new Channel({
+        name: "dispatcher"
+      });
+
+      await channel.publish({
+        event: "foo",
+        data: {
+          foo: "bar"
+        },
+        metadata: {
+          origin: "foo",
+          transactionId: "bar"
+        }
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Unknown event on dispatcher channel"));
+      expect(mockedHandleDispatcherMessages).toHaveBeenCalled();
+      expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
+    });
+
+    test(`Publish a well known event with wrong data,
+          it should log a new Error with the message "Malformed message"`,
+    async() => {
+      const channel = new Channel({
+        name: "dispatcher"
+      });
+
+      await channel.publish({
+        event: "foo",
+        data: {
+          bar: "foo"
+        },
+        metadata: {
+          origin: "foo",
+          transactionId: "bar"
+        }
+      });
+
+      await new Promise((resolve) => setTimeout(resolve, 1_000));
+
+      expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Malformed message"));
+      expect(mockedHandleDispatcherMessages).not.toHaveBeenCalled();
+      expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("Publishing on a dedicated channel", () => {
     const channel = new Channel({
       name: "dispatcher"
     });
-
-    await channel.publish({
-      event: "foo",
-      data: {
-        foo: "bar"
-      },
-      metadata: {
-        origin: "foo",
-        transactionId: "bar"
-      }
-    });
-
-    await new Promise((resolve) => setImmediate(resolve));
-
-    expect(mockedLoggerError).toHaveBeenCalledWith(new Error("Unknown event on dispatcher channel"));
-    expect(mockedHandleDispatcherMessages).toHaveBeenCalled();
-    expect(mockedHandleIncomerMessages).not.toHaveBeenCalled();
   });
 });
 
