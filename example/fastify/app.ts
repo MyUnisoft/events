593ed0743497fb7f394509586eaaa2f2c1df6c06
Nicolas Hallaert <39910164+Rossb0b@users.noreply.github.com>
2023-03-02 14:14:55 +0100

    doc(./example/fastify): enhanced fastify doc with guard for signature & wrong links for json schema (#23)

--- example/fastify/app.ts
+++ example/fastify/app.ts
@@ -1,12 +1,17 @@
+// Import Node.js Dependencies
+import { createHmac } from "crypto";
 
 // Import Third-party Dependencies
-import fastify from "fastify";
+import fastify, { FastifyRequest } from "fastify";
 
 // Import Types
 import { FastifyInstance } from "fastify/types/instance";
 
 // Import Internal Dependencies
 import { webhooksAPI } from "./feature/webhook";
+
+// CONSTANTS
+const kSecret = "foo";
 
 export function buildServer(): FastifyInstance {
   const app = fastify({
@@ -18,10 +23,28 @@ export function buildServer(): FastifyInstance {
     }
   });
 
-  app.register(webhooksAPI, { prefix: "api/v1" });
+  app.register(webhooksAPI, {
+    prefix: "api/v1",
+    preHandler: guard
+  });
 
   return app;
 }
+
+async function guard(req: FastifyRequest): Promise<void> {
+  const { headers, body } = req;
+
+  const { signature, date } = headers;
+
+  const generatedSignature = createHmac("sha256", kSecret)
+    .update(JSON.stringify({ body, date }))
+    .digest("hex");
+
+  if (signature !== generatedSignature) {
+    throw new Error("Wrong signature");
+  }
+}
+
 const server = buildServer();
 
 server.listen({
