84c6f8e0421b22c304a2c67e37332990e531c048
Rossb0b <n.hallaert@myunisoft.fr>
2023-10-04 03:58:45 +0200

    test(): fix ft on event scaling

--- test/UT/class/eventManagement/events.spec.ts
+++ test/UT/class/eventManagement/events.spec.ts
@@ -207,12 +207,15 @@ describe("Publishing/exploiting a custom event", () => {
     let publisher: Incomer;
     let concernedIncomer: Incomer;
     let secondConcernedIncomer: Incomer;
+    let diffConcernedIncomer: Incomer;
     let publisherTransactionStore: TransactionStore<"incomer">;
     let incomerTransactionStore: TransactionStore<"incomer">;
     let secondIncomerTransactionStore: TransactionStore<"incomer">;
+    let diffIncomerTransactionStore: TransactionStore<"incomer">;
     let mockedPublisherSetTransaction;
-    let mockedSecondIncomerSetTransaction;
     let mockedIncomerSetTransaction;
+    let mockedSecondIncomerSetTransaction;
+    let mockedDiffIncomerSetTransaction;
 
     // Constants
     const event: EventOptions<"accountingFolder"> = {
@@ -233,7 +236,7 @@ describe("Publishing/exploiting a custom event", () => {
     afterAll(async() => {
       await publisher.close();
       await concernedIncomer.close();
-      await secondConcernedIncomer.close();
+      await diffConcernedIncomer.close();
     });
 
     beforeAll(async() => {
@@ -254,6 +257,14 @@ describe("Publishing/exploiting a custom event", () => {
       });
 
       secondConcernedIncomer = new Incomer({
+        name: "bar",
+        logger: incomerLogger,
+        eventsCast: [],
+        eventsSubscribe: [{ name: "accountingFolder" }],
+        eventCallback: mockedEventComeBackHandler
+      });
+
+      diffConcernedIncomer = new Incomer({
         name: "foo-bar",
         logger: incomerLogger,
         eventsCast: [],
@@ -320,7 +331,7 @@ describe("Publishing/exploiting a custom event", () => {
 
             concernedIncomer.emit("registered");
           }
-          else {
+          else if (index === 2) {
             Reflect.set(secondConcernedIncomer, "incomerChannelName", data.uuid);
             Reflect.set(secondConcernedIncomer, "providedUUID", data.uuid);
 
@@ -343,9 +354,36 @@ describe("Publishing/exploiting a custom event", () => {
 
             mockedSecondIncomerSetTransaction = jest.spyOn(secondIncomerTransactionStore, "setTransaction");
 
-            Reflect.set(secondConcernedIncomer, "incomerTransactionStore", secondIncomerTransactionStore);
+            Reflect.set(concernedIncomer, "incomerTransactionStore", secondIncomerTransactionStore);
 
-            secondConcernedIncomer.emit("registered");
+            concernedIncomer.emit("registered");
+          }
+          else {
+            Reflect.set(diffConcernedIncomer, "incomerChannelName", data.uuid);
+            Reflect.set(diffConcernedIncomer, "providedUUID", data.uuid);
+
+            diffConcernedIncomer["subscriber"]!.subscribe(data.uuid);
+
+            Reflect.set(diffConcernedIncomer, "incomerChannel", new Channel({
+              name: data.uuid
+            }));
+
+            await updateRegisterTransactionState(
+              diffConcernedIncomer["incomerTransactionStore"],
+              diffConcernedIncomer["registerTransactionId"]!,
+              message.redisMetadata.transactionId
+            );
+
+            diffIncomerTransactionStore = new TransactionStore({
+              prefix: data.uuid,
+              instance: "incomer"
+            });
+
+            mockedDiffIncomerSetTransaction = jest.spyOn(diffIncomerTransactionStore, "setTransaction");
+
+            Reflect.set(diffConcernedIncomer, "incomerTransactionStore", diffIncomerTransactionStore);
+
+            diffConcernedIncomer.emit("registered");
           }
 
           index++;
@@ -354,13 +392,14 @@ describe("Publishing/exploiting a custom event", () => {
       await publisher.initialize();
       await concernedIncomer.initialize();
       await secondConcernedIncomer.initialize();
+      await diffConcernedIncomer.initialize();
 
       await timers.setTimeout(1_600);
 
       await publisher.publish(event);
     });
 
-    test("callback function must have been call & one of the incomers should have create the relating transaction", async() => {
+    test("callback function must have been call & both incomers should have create the relating transaction", async() => {
       await timers.setTimeout(1_600);
 
       expect(mockedPublisherSetTransaction).toHaveBeenCalledWith({
@@ -372,40 +411,21 @@ describe("Publishing/exploiting a custom event", () => {
         relatedTransaction: null
       });
 
+      const mockedEvent = {
+        ...event,
+        redisMetadata: expect.anything(),
+        mainTransaction: false,
+        resolved: false,
+        relatedTransaction: expect.anything()
+    };
+
       if (mockedIncomerSetTransaction.mock.calls.length === 1) {
-        expect(mockedIncomerSetTransaction).toHaveBeenCalledWith({
-            ...event,
-            redisMetadata: expect.anything(),
-            mainTransaction: false,
-            resolved: false,
-            relatedTransaction: expect.anything()
-        });
-        expect(mockedSecondIncomerSetTransaction).not.toHaveBeenCalledWith({
-          ...event,
-          redisMetadata: expect.anything(),
-          mainTransaction: false,
-          resolved: false,
-          relatedTransaction: expect.anything()
-        });
-      }
-      else {
-        expect(mockedIncomerSetTransaction).not.toHaveBeenCalledWith({
-          ...event,
-          redisMetadata: expect.anything(),
-          mainTransaction: false,
-          resolved: false,
-          relatedTransaction: expect.anything()
-        });
-        expect(mockedSecondIncomerSetTransaction).toHaveBeenCalledWith({
-          ...event,
-          redisMetadata: expect.anything(),
-          mainTransaction: false,
-          resolved: false,
-          relatedTransaction: expect.anything()
-        });
+        expect(mockedIncomerSetTransaction).toHaveBeenCalledWith(mockedEvent);
+        expect(mockedSecondIncomerSetTransaction).not.toHaveBeenCalledWith(mockedEvent);
+        expect(mockedDiffIncomerSetTransaction).toHaveBeenCalledWith(mockedEvent);
       }
 
-      expect(mockedEventComeBackHandler).toHaveBeenCalledTimes(1);
+      expect(mockedEventComeBackHandler).toHaveBeenCalledTimes(2);
       expect(mockedEventComeBackHandler).toHaveBeenCalledWith({
         ...event
       });
