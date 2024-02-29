8dab6cb42e5c24475301cb8c8047920688ec34e4
Rossb0b <nicolas.hallaert.pro@gmail.com>
2024-02-29 19:41:33 +0100

    test(registration): updated ut related to hot-reload

--- test/UT/class/eventManagement/registration.spec.ts
+++ test/UT/class/eventManagement/registration.spec.ts
@@ -12,12 +12,13 @@ import {
 import * as Logger from "pino";
 
 // Import Internal Dependencies
-import { Dispatcher, Incomer } from "../../../../src/index";
+import { Dispatcher, EventOptions, Incomer } from "../../../../src/index";
 
 // Internal Dependencies Mocks
 const mockedIncomerHandleDispatcherMessage = jest.spyOn(Incomer.prototype as any, "handleDispatcherMessages");
 const mockedIncomerRegistrationIntervalCb = jest.spyOn(Incomer.prototype as any, "registrationIntervalCb");
 const mockedDispatcherRemoveNonActives = jest.spyOn(Dispatcher.prototype as any, "removeNonActives");
+const mockedIncomerHandleApprovement = jest.spyOn(Incomer.prototype as any, "handleApprovement");
 
 const dispatcherLogger = Logger.pino({
   level: "debug"
@@ -31,6 +32,23 @@ const mockedIncomerLoggerInfo = jest.spyOn(incomerLogger, "info");
 describe("Registration", () => {
   let dispatcher: Dispatcher;
   let incomer: Incomer;
+
+  // CONSTANTS
+  const event: EventOptions<"accountingFolder"> = {
+    name: "accountingFolder",
+    operation: "CREATE",
+    data: {
+      id: "1"
+    },
+    scope: {
+      schemaId: 1,
+      firmId: 1
+    },
+    metadata: {
+      agent: "jest",
+      createdAt: Date.now()
+    }
+  };
 
   function updateIncomerState(...args) {
     incomer["lastPingDate"] = Date.now();
@@ -70,46 +88,10 @@ describe("Registration", () => {
     await clearAllKeys();
   });
 
-  // describe("Initializing consecutively the same Incomer", () => {
-  //   const eventComeBackHandler = () => void 0;
-
-  //   afterAll(async() => {
-  //     await incomer.close();
-  //   });
-
-  //   beforeAll(async() => {
-  //     incomer = new Incomer({
-  //       name: "foo",
-  //       logger: incomerLogger,
-  //       eventsCast: [],
-  //       eventsSubscribe: [],
-  //       eventCallback: eventComeBackHandler
-  //     });
-
-  //     await incomer.initialize();
-  //   });
-
-  //   it("Should correctly register the new incomer", async() => {
-  //     await timers.setTimeout(1_600);
-
-  //     expect(mockedIncomerHandleDispatcherMessage).toHaveBeenCalled();
-  //     expect(mockedIncomerLoggerInfo.mock.calls[2][0]).toContain("Incomer registered");
-  //   });
-
-  //   test("Calling Incomer initialize a second time, it should fail", async() => {
-  //     expect.assertions(1);
-
-  //     try {
-  //       await incomer.initialize()
-  //     }
-  //     catch (error) {
-  //       expect(error.message).toBe("Cannot init multiple times.");
-  //     }
-  //   });
-  // });
-
   describe("Initializing a new Incomer", () => {
-    let handlePingFn;
+    let handlePingFn: (...any) => any;
+    let incomerProvidedUUID: string;
+
     const eventComeBackHandler = () => void 0;
 
     afterAll(async() => {
@@ -141,7 +123,11 @@ describe("Registration", () => {
 
       expect(mockedIncomerHandleDispatcherMessage).toHaveBeenCalled();
       expect(mockedIncomerLoggerInfo.mock.calls[2][0]).toContain("Incomer registered");
+      expect(mockedIncomerHandleApprovement).toHaveBeenCalledTimes(1);
       expect(mockedIncomerRegistrationIntervalCb).toHaveBeenCalledTimes(0);
+      expect(incomer["providedUUID"]).toBeDefined();
+
+      incomerProvidedUUID = incomer["providedUUID"];
     });
 
     it("Should have removed the incomer", async() => {
@@ -150,12 +136,30 @@ describe("Registration", () => {
       expect(mockedDispatcherRemoveNonActives).toHaveBeenCalled();
 
       Reflect.set(incomer, "handlePing", handlePingFn);
+      expect(incomer.dispatcherConnectionState).toBe(false);
+      await incomer.publish(event);
+
+      const incomerTransactions = await incomer["newTransactionStore"].getTransactions();
+
+      expect([...incomerTransactions.values()]).toContainEqual(expect.objectContaining({
+        name: event.name
+      }));
     });
 
-    it("Should have register again", async() => {
+    it("Should have register again & handle the unpublished event", async() => {
       await timers.setTimeout(2_000);
       expect(mockedIncomerRegistrationIntervalCb).toHaveBeenCalled();
       expect(incomer.dispatcherConnectionState).toBe(true);
+      await timers.setTimeout(2_000);
+
+      expect(mockedIncomerHandleApprovement).toHaveBeenCalledTimes(2);
+      expect(incomer["providedUUID"]).toBe(incomerProvidedUUID);
+
+      const incomerTransactions = await incomer["newTransactionStore"].getTransactions();
+
+      expect([...incomerTransactions.values()]).not.toContainEqual(expect.objectContaining({
+        name: event.name
+      }));
     });
   });
 });
