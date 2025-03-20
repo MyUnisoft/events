f05e30f258cf96d28464bccde2bd291361409f1e
Rossb0b <nicolas.hallaert.pro@gmail.com>
2025-03-20 11:14:06 +0100

    feat(./class/service/events): service to get data on events

---
+++ src/class/service/events.service.ts
@@ -0,0 +1,165 @@
+// Import Third-party Dependencies
+import { RedisAdapter, type Types } from "@myunisoft/redis";
+
+// Import Internal Dependencies
+import { IncomerStore } from "../store/incomer.class.js";
+import { Transaction, TransactionStore } from "../store/transaction.class.js";
+import type { Events } from "../../types/index.js";
+
+export interface EventsServiceOptions {
+  redis: Types.DatabaseConnection<RedisAdapter>;
+  incomerStore: IncomerStore;
+  dispatcherTransactionStore: TransactionStore<"dispatcher">;
+  backupDispatcherTransactionStore: TransactionStore<"dispatcher">;
+  backupIncomerTransactionStore: TransactionStore<"incomer">;
+}
+
+interface SharedOptions {
+  incomerId: string;
+}
+
+export type GetEventById = SharedOptions & {
+  eventId: string;
+};
+
+export type GetSentEventByIdResponse = Omit<Transaction<"incomer">, "redisMetadata"> & {
+  redisMetadata: {
+    published: boolean;
+    resolved: boolean;
+    relatedTransactions: string[];
+  }
+} | null;
+
+export type GetIncomerSendEventsResponse = GetSentEventByIdResponse[];
+
+export type GetEventsByName = SharedOptions & {
+  name: keyof Events;
+};
+
+export class EventsService {
+  #redis: Types.DatabaseConnection<RedisAdapter>;
+  private incomerStore: IncomerStore;
+  private dispatcherTransactionStore: TransactionStore<"dispatcher">;
+  private backupDispatcherTransactionStore: TransactionStore<"dispatcher">;
+  private backupIncomerTransactionStore: TransactionStore<"incomer">;
+
+  constructor(opts: EventsServiceOptions) {
+    Object.assign(this, opts);
+
+    this.#redis = opts.redis;
+  }
+
+  async getIncomers() {
+    return await this.incomerStore.getIncomers();
+  }
+
+  async getEventById(opts: GetEventById): Promise<GetSentEventByIdResponse> {
+    const { incomerId, eventId } = opts;
+
+    const incomerTransactionStore = new TransactionStore({
+      adapter: this.#redis,
+      prefix: incomerId,
+      instance: "incomer"
+    });
+
+    let mainTransaction = await incomerTransactionStore.getTransactionById(eventId);
+
+    if (mainTransaction === null) {
+      mainTransaction = await this.backupIncomerTransactionStore.getTransactionById(eventId);
+    }
+
+    return mainTransaction === null ? null : {
+      ...mainTransaction,
+      redisMetadata: {
+        published: mainTransaction.redisMetadata.published,
+        resolved: mainTransaction.redisMetadata.resolved,
+        relatedTransactions: mainTransaction.redisMetadata.relatedTransaction
+      }
+    };
+  }
+
+  async getIncomerSentEvents(opts: SharedOptions): Promise<GetIncomerSendEventsResponse> {
+    const { incomerId } = opts;
+
+    const incomerTransactionStore = new TransactionStore({
+      adapter: this.#redis,
+      prefix: incomerId,
+      instance: "incomer"
+    });
+
+    const mainTransactions = await incomerTransactionStore.getTransactions();
+
+    return [...mainTransactions.values()]
+      .map((mainTransaction) => {
+        if (mainTransaction) {
+          return {
+            ...mainTransaction,
+            redisMetadata: {
+              published: mainTransaction.redisMetadata.published,
+              resolved: mainTransaction.redisMetadata.resolved,
+              relatedTransactions: mainTransaction.redisMetadata.relatedTransaction
+            }
+          };
+        }
+
+        return null;
+      });
+  }
+
+  async getIncomerSentEventsByName(opts: GetEventsByName): Promise<GetIncomerSendEventsResponse> {
+    const { incomerId, name } = opts;
+
+    const incomerTransactionStore = new TransactionStore({
+      adapter: this.#redis,
+      prefix: incomerId,
+      instance: "incomer"
+    });
+
+    const mainTransactions = await incomerTransactionStore.getTransactions();
+
+    return [...mainTransactions.values()]
+      .flatMap((mainTransaction) => {
+        if (mainTransaction.name === name) {
+          return {
+            ...mainTransaction,
+            redisMetadata: {
+              published: mainTransaction.redisMetadata.published,
+              resolved: mainTransaction.redisMetadata.resolved,
+              relatedTransactions: mainTransaction.redisMetadata.relatedTransaction
+            }
+          };
+        }
+
+        return [];
+      });
+  }
+
+  async getIncomerReceivedEvents(opts: SharedOptions) {
+    const { incomerId } = opts;
+
+    const dispatcherTransactions = await this.dispatcherTransactionStore.getTransactions();
+    const backupDispatcherTransactions = await this.backupDispatcherTransactionStore.getTransactions();
+
+    const mappedBackupDispatcherTransactions = [...backupDispatcherTransactions.values()].map((backupDispatcherTransaction) => {
+      return {
+        ...backupDispatcherTransaction,
+        isBackupTransaction: true
+      };
+    });
+
+    const receivedEvents = [];
+    for (const dispatcherTransaction of [...dispatcherTransactions.values(), ...mappedBackupDispatcherTransactions]) {
+      if (dispatcherTransaction.redisMetadata.to === incomerId) {
+        receivedEvents.push({
+          ...dispatcherTransaction,
+          redisMetadata: {
+            eventTransactionId: dispatcherTransaction.redisMetadata.eventTransactionId,
+            resolved: dispatcherTransaction.redisMetadata.resolved
+          }
+        });
+      }
+    }
+
+    return receivedEvents;
+  }
+}
